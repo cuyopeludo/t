8a69d3aea2e23dfb357a56c57c26150ea0782c3d
cuyopeludo <120688547+cuyopeludo@users.noreply.github.com>
2023-07-03 18:51:41 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+Webcam.attach( '#camera' );
+
+camera = document.getElementById("camera");
+      
+  Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+  });
+
+
+
+
+
